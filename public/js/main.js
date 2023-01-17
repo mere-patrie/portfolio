95f1e639a450c4ae55e9b94348d7d5abd53c737f
Angus <angus.paillaugue40@gmail.com>
2023-01-17 18:01:09 +0100

    a

--- public/js/main.js
+++ public/js/main.js
@@ -61,3 +61,45 @@ function scroll() {
         }
     });
 }
+
+function sendMessage() {
+    const email = $("#email").val();
+    const name = $("#name").val();
+    const subject = $("#subject").val();
+    const message = $("#message").val();
+    $("#formError").hide();
+    if(isEmailValid(email)){
+        if(name.length > 0){
+            if(subject.length > 0){
+                if(message.length > 0){
+                    $.post("/mail", {email, name, subject, message}, (data) => {
+                        console.log(data);
+                    })
+                }else{
+                    new formError("Please enter a message").throw();
+                }
+            }else{
+                new formError("Please provide a subject").throw();
+            }
+        }else{
+            new formError("Please provide a name").throw();
+        }
+    }else{
+        new formError("Invalid e-mail").throw();
+    }
+}
+
+function isEmailValid(email) {
+    const regex =  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
+    return regex.test(email);
+}
+
+class formError {
+    constructor(errorText){
+        this.errorText = errorText;
+    }
+    throw() {
+        $("#formError").show();
+        $("#formError").html(`<div><span> ${this.errorText}</span></div>`);
+    }
+}
