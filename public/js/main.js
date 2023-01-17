9f8596334fec591dc46fe2c52e3491de8f146fe0
Angus <angus.paillaugue40@gmail.com>
2023-01-17 16:26:05 +0100

    a

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-
 function myAge(){
     const now = new Date();
     const birthDate = new Date(2005, 9, 14);
@@ -8,8 +7,12 @@ function myAge(){
 }
 
 function landingPageButtonsScroll() {
+    if(localStorage.getItem("section")){
+        $(`#${localStorage.getItem("section")}`)[0].scrollIntoView({ behavior: 'smooth' });
+    }
     $("[data-scroll]").click((e) => {
         $("#"+$(e.target)[0].closest(".buttonGridButton").getAttribute("data-scroll"))[0].scrollIntoView({ behavior: 'smooth' })
+        localStorage.setItem('section', $(e.target)[0].closest(".buttonGridButton").getAttribute("data-scroll"));
     });
 }
 
@@ -21,11 +24,40 @@ function main() {
 $("document").ready(function() {
     main();
     landingPageButtonsScroll();
+    scroll()
 });
 
-$("#pageContainer").scroll(function() {
-    var opacity = ($("#landingPageLogo").parent().offset().top+$("#landingPageLogo").parent().innerHeight()/2)/($("#landingPageLogo").parent().height()/2);
-    opacity = opacity >= 1 ? 1 : opacity;
-    opacity = opacity <= 0 ? 0 : opacity;
-    $("#landingPageLogo").css("opacity", opacity);
-});
+function scroll() {
+    $("#pageContainer").scroll(function() {
+        // Logo opacity
+        var opacity = ($("#landingPageLogo").parent().offset().top+$("#landingPageLogo").parent().height()/2)/($("#landingPageLogo").parent().height()/2);
+        opacity = opacity >= 1 ? 1 : opacity;
+        opacity = opacity <= 0 ? 0 : opacity;
+        $("#landingPageLogo").css("opacity", opacity);
+        
+        // Section url params
+        var elements = [{name:"", position:0}];
+        var goal = $("#pageContainer").scrollTop();
+        
+        // Appending every section name and position to array
+        $("[data-scroll]").each(index => {
+            var name = $("[data-scroll]")[index].getAttribute("data-scroll");
+            var position = $("#"+name).height()*(index+1);
+            elements.push({name:name, position:position})
+        });
+        
+        // Getting closest section
+        var closest = Object.values(elements).reduce(function(prev, curr) {
+            return (Math.abs(curr.position - goal) < Math.abs(prev.position - goal) ? curr : prev);
+        });
+        
+        // Setting the current section to localStorage
+        if(localStorage.getItem("section") != closest.name){
+            if(closest.name == ""){
+                localStorage.removeItem("section");
+            }else{
+                localStorage.setItem("section", closest.name);
+            }
+        }
+    });
+}
